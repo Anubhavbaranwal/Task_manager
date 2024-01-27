5f3a505236dbba84abd358c526139c68408c9b3a
Anubhav Baranwal <anubhavbaranwal08@gmail.com>
2024-01-27 21:35:51 +0530

    register page created

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,85 @@
+// import axios from "axios";
+// import React, { useState } from "react";
+// import img from "../assets/20945760.jpg";
+// import profile from "../assets/profile_4945750.png";
+
+// const Login = () => {
+//   const [data, setData] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     await axios
+//       .post("http://localhost:5643/api/v1/Login", data, {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data),
+//       })
+//       .then((response) => {
+//         console.log(response);
+//         setData({
+//           email: "",
+//           password: "",
+//         });
+//         alert("Login Succefully");
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+//   const handlechange = (e) => {
+//     let name = e.target.name;
+//     let value = e.target.value;
+//     setData({
+//       ...data,
+//       [name]: value,
+//     });
+//   };
+//   return (
+//     <div className="flex m-4">
+//       <div className="w-1/2 items-center flex justify-center ">
+//         <img
+//           src={img}
+//           alt="login page image"
+//           className="flex justify-center  w-4/5"
+//         />
+//       </div>
+//       <div className=" w-1/2 flex flex-col justify-center ">
+//         <h1 className="text-[#7D97F4] justify-center flex text-5xl my-6 gap-x-1">
+//           Login
+//         </h1>
+//         <form
+//           onSubmit={handleSubmit}
+//           className=" items-center flex flex-col p-2 gap-y-4  "
+//         >
+//           <input
+//             type="text"
+//             placeholder="email"
+//             name="email"
+//             className="w-1/2 h-10 rounded-md p-2 bg-[#1a1a1a] my-4"
+//             value={data.email}
+//             onChange={handlechange}
+//           />
+//           <input
+//             type="password"
+//             placeholder="password"
+//             name="password"
+//             className="w-1/2 h-10 text-white  rounded-md p-2 bg-[#1a1a1a] my-2"
+//             value={data.password}
+//             onChange={handlechange}
+//           />
+//           <button
+//             type="submit"
+//             className="my-4 rounded-xl bg-[#7D97F4] w-1/3 p-1"
+//           >
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Register = () => {
+  const [logged, setLogged] = useState(false);
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    setData({
+      ...data,
+      [name]: value,
+    });
+    console.log(data);
+  };
+  const handlesubmit = async () => {
+    await axios
+      .post("http://localhost:5643/api/v1/register", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+      .then((response) => {
+        console.log(response);
+        setData({
+          username: "",
+          email: "",
+          password: "",
+        });
+        alert("registered successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="w-4/5 mx-4 md:w-1/3 my-12 border items-center rounded-xl p-5 ">
+      <h1 className=" text-5xl font-semibold text-center text-slate-400">
+        Register
+      </h1>
+      <div className="p-4 mx-5 items-center">
+        <label htmlFor="username" className=" text-white font-medium pb-4 pl-1">
+          Username
+        </label>
+        <br />
+        <input
+          value={data.username}
+          onChange={handleInput}
+          type="text"
+          id="username"
+          name="username"
+          className="px-2 rounded-lg w-full h-9"
+        />
+      </div>
+      <div className="p-4 mx-5 items-center">
+        <label htmlFor="email" className=" text-white font-medium pb-6 pl-1">
+          Email
+        </label>
+        <br />
+        <input
+          onChange={handleInput}
+          type="text"
+          id="email"
+          value={data.email}
+          name="email"
+          className="px-2 rounded-lg w-full h-9"
+        />
+      </div>
+      <div className="p-4 mx-5 items-center">
+        <label htmlFor="password" className=" text-white font-medium pb-7 pl-1">
+          password
+        </label>
+        <br />
+        <input
+          onChange={handleInput}
+          value={data.password}
+          type="text"
+          id="password"
+          name="password"
+          className="rounded-lg w-full h-9"
+        />
+      </div>
+      <button
+        className="p-2 mx-11 my-5  w-4/5 rounded-3xl  bg-zinc-600"
+        onClick={handlesubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default Register;
